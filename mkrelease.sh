e2e98eaabf04786c8d7c0593a9e79507c7a0beca
Jim (encrypted) <jimcrypt@du-069-0025.access.clara.net>
2006-01-25 22:25:42 +0000

    Show old tag; fix changelog version stuff; changelog updates to top

--- mkrelease.sh
+++ mkrelease.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 old_tag=`git describe | sed 's/-g.*//'`
-echo "$old_tag:" >> Changelog
-git log --pretty=short $old_tag..HEAD | git shortlog >> Changelog
+echo Old tag is $old_tag
 echo New tag?
 read new_tag
+echo "$new_tag" >> Changelog.new
+git log --pretty=short $old_tag..HEAD | git shortlog >> Changelog.new
+cat Changelog >> Changelog.new
+mv Changelog.new Changelog
 git commit -m 'Release '$new_tag Changelog
 git tag -a -m $new_tag $new_tag
